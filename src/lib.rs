110196738d3d13f84810855850d9a7b69620aa1b
Dabo Ross <daboross@daboross.net>
2015-06-19 18:45:03 -0700

    Update a bit, add some more tests, add some documentation!

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,107 @@
-use std::error;
-use std::fmt;
+/// Throw!
+/// ------
+///
+/// Throw is a new experimental rust error handling library, meant to assist and build on existing
+/// error handling systems.
+///
+/// Throw exports two structs, `throw::ErrorPoint` and `throw::Error`. `throw::Error` stores a
+/// single `original_error` variable which it is created from, and then a list of `ErrorPoint`s
+/// which is added to every time you `throw!()` the error to a higher function.
+///
+/// *Throw does not replace existing error handling systems*. Instead, you will want to replace
+/// functions which return `Result<T, MyError>` with `Result<T, throw::Error<MyError>>` for full
+/// affect.
+///
+/// The main way you use throw is through the `throw!()` macro, which acts exactly like `try!()`
+/// except specifically for functions returning a `Result<T, throw::Error<E>>` rather than any
+/// `Result<T, E>`. You can `throw!()` a regular `Result<T, E>`, but the function within which you
+/// use `throw!()` must return `Result<T, throw::Error<E>>`.
+///
+/// Here's an example of throw in action:
+///
+/// ```rust
+/// #[macro_use]
+/// extern crate throw;
+///
+/// use std::io::prelude::*;
+/// use std::io;
+/// use std::fs::File;
+///
+/// fn read_log() -> Result<String, throw::Error<io::Error>> {
+///     let mut file = throw!(File::open("some_file.log"));
+///     let mut buf = String::new();
+///     throw!(file.read_to_string(&mut buf));
+///     Ok((buf))
+/// }
+///
+/// fn do_things() -> Result<(), throw::Error<io::Error>> {
+///     let log_contents = throw!(read_log());
+///     println!("Log contents: {}", log_contents);
+///
+///     Ok(())
+/// }
+///
+/// fn main() {
+///     let result = do_things();
+///     if let Err(e) = result {
+/// #       /*
+///         panic!("{}", e);
+/// #       */
+/// #       assert_eq!(format!("{}", e), "Error: No such file or directory (os error 2)\
+/// #       \n\tat 16:23 in rust_out (<anon>)\
+/// #       \n\tat 9:19 in rust_out (<anon>)");
+///     }
+/// }
+/// ```
+///
+/// This simple program behaves exactly as if `Result<_, io::Error>` directly when it functions
+/// correctly. When the program encounters is when throw really shines. Instead of a simple `No
+/// such file or directory` message, you get:
+///
+/// ```text
+/// Error: No such file or directory (os error 2)
+///    at 16:23 in main (src/main.rs)
+///    at 9:19 in main (src/main.rs)
+/// ```
+///
+/// These stack traces are stored inside throw::Error, and are recorded automatically when
+/// `throw!()` returns an Err value.
+///
+/// ---
+///
+/// Throwing directly from a function is also supported, using `throw_new!()`:
+///
+/// ```
+/// # #[macro_use]
+/// # extern crate throw;
+/// fn possibly_fails() -> Result<(), throw::Error<&'static str>> {
+///     if true {
+///         // throw_new!() will always return directly
+///         throw_new!("oops");
+///     }
+///
+///     Ok(())
+/// }
+///
+/// fn main() {
+/// #   /*
+///     possibly_fails().unwrap()
+/// #   */
+/// #   assert_eq!(format!("{}", possibly_fails().unwrap_err()), "Error: oops\
+/// #   \n\tat 6:8 in rust_out (<anon>)")
+/// }
+/// ```
+///
+/// ```text
+/// called `Result::unwrap()` on an `Err` value: Error: "oops"
+///    at 6:8 in main (src/main.rs)
+/// ```
+///
+/// `throw!()` can also be used in place of `throw_new!()` *if the argument provided is &str or
+/// String. `throw!()` is special cased so that if you provide it a string literal rather than a
+/// Result, it will throw the string literal directly.
 
-pub type BoxedStdError = Box<error::Error + Sync + Send>;
+use std::fmt;
 
 pub struct ErrorPoint {
     pub line: u32,
@@ -10,29 +110,29 @@ pub struct ErrorPoint {
     pub file: &'static str,
 }
 
-pub struct Error {
+pub struct Error<E> {
     points: Vec<ErrorPoint>,
-    original_error: BoxedStdError,
+    original_error: E,
 }
 
-impl Error {
+impl<E> Error<E> {
+    #[doc(hidden)]
+    pub fn __push_point(&mut self, point: ErrorPoint) {
+        self.points.push(point);
+    }
+
     pub fn points(&self) -> &[ErrorPoint] {
         &self.points
     }
 
-    pub fn original_error(&self) -> &BoxedStdError {
+    pub fn original_error(&self) -> &E {
         &self.original_error
     }
 }
 
-impl fmt::Display for Error {
+impl<E> fmt::Display for Error<E> where E: fmt::Display {
     fn fmt(&self, fmt: &mut fmt::Formatter) -> Result<(), fmt::Error> {
-        try!(write!(fmt, "Error: {}", self.original_error.description()));
-        let mut cause: &error::Error = &*self.original_error;
-        while let Some(e) = cause.cause() {
-            cause = e;
-            try!(write!(fmt, "\n\t (reported cause: {})", e.description()));
-        }
+        try!(write!(fmt, "Error: {}", self.original_error));
         for point in self.points.iter().rev() {
             try!(write!(fmt, "\n\tat {}:{} in {} ({})", point.line, point.column,
                 point.module, point.file));
@@ -42,49 +142,63 @@ impl fmt::Display for Error {
     }
 }
 
-pub trait ThrowBehavior {
-    type OkResult;
+impl<E> fmt::Debug for Error<E> where E: fmt::Debug {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        try!(write!(fmt, "Error: {:?}", self.original_error));
+        for point in self.points.iter().rev() {
+            try!(write!(fmt, "\n\tat {}:{} in {} ({})", point.line, point.column,
+                point.module, point.file));
+        }
 
-    fn handle_throw(self, point: ErrorPoint) -> Result<Self::OkResult, Error>;
+        Ok(())
+    }
 }
 
-impl<T, E> ThrowBehavior for Result<T, E> where E: Into<BoxedStdError> {
-    type OkResult = T;
+impl <E> From<E> for Error<E> {
+    fn from(error: E) -> Error<E> {
+        Error {
+            points: Vec::new(),
+            original_error: error,
+        }
+    }
+}
 
-    fn handle_throw(self, point: ErrorPoint) -> Result<T, Error> {
+pub trait ThrowBehavior<E> {
+    type OkType;
+
+    fn handle_throw(self) -> Result<Self::OkType, Error<E>>;
+}
+
+impl<T, OE, NE> ThrowBehavior<NE> for Result<T, OE> where OE: Into<Error<NE>> {
+    type OkType = T;
+
+    fn handle_throw(self) -> Result<T, Error<NE>> {
         match self {
             Ok(x) => Ok(x),
             Err(e) => {
-                Err(Error {
-                    points: vec![point],
-                    original_error: e.into(),
-                })
+                Err(e.into())
             }
         }
     }
 }
 
-impl<T> ThrowBehavior for Result<T, Error> {
-    type OkResult = T;
+impl<'a, E> ThrowBehavior<E> for &'a str where &'a str: Into<E> {
+    type OkType = ();
 
-    fn handle_throw(self, point: ErrorPoint) -> Result<T, Error> {
-        match self {
-            Ok(x) => Ok(x),
-            Err(mut e) => {
-                e.points.push(point);
-                Err(e)
-            }
-        }
-    }
-}
-
-impl ThrowBehavior for &'static str {
-    type OkResult = ();
-
-    fn handle_throw(self, point: ErrorPoint) -> Result<(), Error> {
+    fn handle_throw(self) -> Result<(), Error<E>> {
         Err(Error {
-            points: vec![point],
-            original_error: self.to_owned().into(),
+            points: Vec::new(),
+            original_error: self.into(),
+        })
+    }
+}
+impl<E> ThrowBehavior<E> for String where String: Into<E> {
+    type OkType = ();
+
+    fn handle_throw(self) -> Result<(), Error<E>> {
+        Err(Error {
+            points: Vec::new(),
+            original_error: self.into(),
         })
     }
 }
@@ -92,14 +206,31 @@ impl ThrowBehavior for &'static str {
 #[macro_export]
 macro_rules! throw {
     ($e:expr) => (
-        match $crate::ThrowBehavior::handle_throw($e, $crate::ErrorPoint {
+        match $crate::ThrowBehavior::handle_throw($e) {
+            Ok(v) => v,
+            Err(mut e) => {
+                e.__push_point($crate::ErrorPoint {
+                    line: line!(),
+                    column: column!(),
+                    module: module_path!(),
+                    file: file!(),
+                });
+                return Err(e);
+            },
+        }
+    )
+}
+
+#[macro_export]
+macro_rules! throw_new {
+    ($e:expr) => ({
+        let mut e = $crate::Error::from($e);
+        e.__push_point($crate::ErrorPoint {
             line: line!(),
             column: column!(),
             module: module_path!(),
             file: file!(),
-        }) {
-            Ok(v) => v,
-            Err(e) => return Err(e),
-        }
-    )
+        });
+        return Err(e);
+    })
 }
--- tests/lib.rs
+++ tests/lib.rs
@@ -2,15 +2,54 @@
 extern crate throw;
 
 
-fn throw_static_message() -> Result<(), throw::Error> {
+fn throw_static_message() -> Result<(), throw::Error<&'static str>> {
     throw!("hi");
 
     Ok(())
 }
 
+fn throw1() -> Result<(), throw::Error<()>> {
+    throw_new!(());
+}
+
+fn throw2() -> Result<(), throw::Error<()>> {
+    throw!(throw1());
+    Ok(())
+}
+
+fn throw3() -> Result<(), throw::Error<()>> {
+    throw!(throw2());
+    Ok(())
+}
+
+fn gives_ok() -> Result<&'static str, throw::Error<&'static str>> {
+    Ok("ok")
+}
+
+fn throws_ok() -> Result<&'static str, throw::Error<&'static str>> {
+    let ok_msg = throw!(gives_ok());
+    Ok(ok_msg)
+}
+
 #[test]
 fn test_static_message() {
     let error = throw_static_message().unwrap_err();
-    assert_eq!(error.original_error().to_string(), "hi");
-    assert_eq!("Error: hi\n\tat 6:4 in lib (tests/lib.rs)", format!("{}", error));
+    assert_eq!(*error.original_error(), "hi");
+    assert_eq!(format!("{}", error), "Error: hi\n\tat 6:4 in lib (tests/lib.rs)");
+}
+
+#[test]
+fn test_multiple_throws() {
+    let error = throw3().unwrap_err();
+    assert_eq!(error.original_error(), &());
+    assert_eq!(format!("{:?}", error), "Error: ()\
+    \n\tat 21:4 in lib (tests/lib.rs)\
+    \n\tat 16:4 in lib (tests/lib.rs)\
+    \n\tat 12:4 in lib (tests/lib.rs)");
+}
+
+#[test]
+fn test_returns_ok() {
+    let ok = throws_ok().unwrap();
+    assert_eq!(ok, "ok");
 }
