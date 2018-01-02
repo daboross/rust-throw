788a8b5ebe18f89b1c7fe99c6043d2ddb692b86c
David Ross <daboross@daboross.net>
2018-01-01 19:50:31 -0800

    Update tests to work on latest nightly where line numbers shifted by one.

--- tests/exceptions_work.rs
+++ tests/exceptions_work.rs
@@ -1,8 +1,20 @@
+extern crate regex;
 #[macro_use]
 extern crate throw;
 
 use throw::Result;
 
+macro_rules! assert_matches {
+    ($expected:expr, $actual:expr) => ({
+        let expected = ($expected).replace("    ", "\t");
+        let re = regex::Regex::new(&expected).expect("expected hardcoded regex to compile");
+
+        let actual = format!("{}", $actual);
+
+        assert!(re.is_match(&actual),
+            format!("expected error to match regex `\n{}\n`, but found `\n{}\n`", expected, actual));
+    })
+}
 
 fn throw_static_message() -> Result<(), &'static str> {
     throw_new!("hi");
@@ -48,7 +60,8 @@ fn throws_into() -> Result<(), String> {
 fn test_static_message() {
     let error = throw_static_message().unwrap_err();
     assert_eq!(*error.err(), "hi");
-    assert_eq!(error.to_string(), "Error: hi\n\tat 8:4 in lib (tests/lib.rs)");
+    assert_matches!(r#"Error: hi
+    at [0-9]+:[0-9] in exceptions_work \([a-z/._-]+\)"#, error);
     assert_eq!("hi".to_owned(), error.into_err::<String>());
 }
 
@@ -58,10 +71,13 @@ fn test_multiple_throws() {
     let error = throw3().unwrap_err();
     assert_eq!(error.err(), &());
     assert_eq!(error.err(), error.original_error());
-    assert_eq!(format!("{:?}", error), "Error: ()\
-    \n\tat 21:4 in lib (tests/lib.rs)\
-    \n\tat 16:4 in lib (tests/lib.rs)\
-    \n\tat 12:4 in lib (tests/lib.rs)");
+    assert_matches!(
+        r#"Error: \(\)
+    at [0-9]+:[0-9] in exceptions_work \([a-z/._-]+\)
+    at [0-9]+:[0-9] in exceptions_work \([a-z/._-]+\)
+    at [0-9]+:[0-9] in exceptions_work \([a-z/._-]+\)"#,
+        format!("{:?}", error)
+    );
 }
 
 #[test]
@@ -73,13 +89,19 @@ fn test_returns_ok() {
 #[test]
 fn test_mod_throw() {
     let error = mod_test::throws().unwrap_err();
-    assert_eq!(error.to_string(), "Error: ahhhh\
-    \n\tat 38:8 in lib::mod_test (tests/lib.rs)");
+    assert_matches!(
+        r#"Error: ahhhh
+    at [0-9]+:[0-9] in exceptions_work::mod_test \([a-z/._-]+\)"#,
+        error
+    );
 }
 
 #[test]
 fn test_throws_into() {
     let error = throws_into().unwrap_err();
-    assert_eq!(error.to_string(), "Error: some static string\
-    \n\tat 43:4 in lib (tests/lib.rs)")
+    assert_matches!(
+        r#"Error: some static string
+    at [0-9]+:[0-9] in exceptions_work \([a-z/._-]+\)"#,
+        error
+    )
 }
